375461f623bda3df4edd99a97fe5e05623bcc755
Akbarjon03 <akbarjonodilov03@gmail.com>
2023-06-01 16:54:07 +0500

    #

--- server/routes/router.js
+++ server/routes/router.js
@@ -7,5 +7,4 @@ router.post('/login',  adminController.login)
 router.post('/register', UserMiddleware, adminController.register)
 router.post('/userInfo', UserMiddleware, adminController.userinfo)
 
-
 module.exports = router
